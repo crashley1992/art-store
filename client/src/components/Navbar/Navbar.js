e4acae99e2821fe15cc183bd435e99607944baf7
Ashley Clarke <crashley92@gmail.com>
2020-06-11 21:50:22 -0700

    changed up navbar and added icons

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
+import { FaShoppingCart } from "react-icons/fa";
+import { FaStore } from "react-icons/fa";
 
 const Navbar = () => {
     return (
         <nav className="nav-wrapper">
             <div className="container">
-                <Link to="/" className="brand-logo left">Obement Art</Link>
+                <Link to="/" className="brand-logo">Obement Art</Link>
 
-                <ul className="right">
-                    <li><Link to="/">Shop</Link></li>
-                    <li><Link to="/cart">My Cart</Link></li>
-                    <li><Link to="/cart"><i className="material-icons">shopping_cart</i></Link></li>
+                <ul className="nav-links">
+                    <li className="nav-item"><Link to="/">Shop <FaStore size={25} className="shopping-icon"/></Link></li>
+                    <li className="nav-item"><Link to="/cart">Checkout<FaShoppingCart size={25} className="shopping-icon"/></Link></li>
                 </ul>
             </div>
         </nav>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
 class Home extends Component {
     render() {
-        return(
-            <div>
-                Home
-            </div>
+        return(            
+        <div>
+        
+        </div>
+            
         )
     }
 }
